682bf1eac5f4a1c26b1a122e8a46ad2a0e6f9f2c
KorolevAndrey <44860813+KorolevAndrey@users.noreply.github.com>
2019-04-22 19:11:16 +0300

    2
    2

--- desktop/src/com/mygdx/game9876/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game9876/desktop/DesktopLauncher.java
@@ -2,13 +2,14 @@ package com.mygdx.game9876.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game9876.MyGdxGame876;
+import com.mygdx.game9876.PlatformerTutorial;
+
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1400;
-		config.height = 1000;
-		new LwjglApplication(new MyGdxGame876(), config);
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new PlatformerTutorial(), config);
 	}
 }
